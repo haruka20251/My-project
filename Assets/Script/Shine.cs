997cdbf0e4e101b733aa0d25537407833d1a5fd6
haruka20251 <h10m3d5contrail@gmail.com>
2025-03-03 15:05:19 +0900

    発光機能の追加

---
+++ Assets/Script/Shine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shine : MonoBehaviour
+{
+    public Material myMaterial;
+    public Material myMaterial2;
+
+    private Color defaultColor = Color.red;
+    private float elapsedTime = 0f;
+
+    void Start()
+    {
+        this.defaultColor = Color.red;
+
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        Debug.Log(elapsedTime);
+
+        if (elapsedTime >= 20f)
+        {
+            if (elapsedTime < 21f)
+            {
+
+                Color fixedColor = this.defaultColor * 20.0f;
+                
+                myMaterial.SetColor("_EmissionColor", fixedColor);
+
+                
+                myMaterial2.SetColor("_EmissionColor", fixedColor);
+            }
+        }
+    }
+}
